5e3ccd5533e449e704256fbafff574df79f59dda
ItsKristian01 <kristianhana11@gmail.com>
2023-11-22 15:10:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <h1>Ciao ecco:</h1>
+  <AppTitle />
+</template>
+
+<style scoped>
+</style>
